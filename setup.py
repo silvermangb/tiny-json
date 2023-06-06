dc8ac08b5af417ee80f211ccbc1a57e99d9b9e10
Greg Silverman <silvermangb@gmail.com>
2023-06-06 16:35:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from setuptools import setup, find_packages
 
-long_description = None
 with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(
-    name="tiny-json",
-    version="1.0",
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    description="Encode a JSON object to a small CSV string and decode that to the original JSON.",
-    author="Greg  Silverman",
-    author_email="silvermangb@gmail.com",
-    install_requires=[],
-    package_dir={"tiny-json": "src"},
-)
+    setup(
+        name="tiny-json",
+        version="1.0",
+        packages=find_packages(),
+        long_description=fh.read(),
+        long_description_content_type="text/markdown",
+        description="Encode a JSON object to a small CSV string and decode that to the original JSON.",
+        author="Greg  Silverman",
+        author_email="silvermangb@gmail.com",
+        install_requires=[],
+        package_dir={"tiny-json": "src"},
+    )
 
